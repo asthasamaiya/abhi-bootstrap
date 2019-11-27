9749644c4e4618bcfd9ebf94b9b3c682676ae3c4
asthasamaiya <astha.samaiya@hotmail.com>
2019-11-27 18:27:06 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LibraryComponent } from './library/library.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: '', component : HomeComponent},
+    {path: 'home', component : HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'library', component: LibraryComponent},
+    {path: 'contact', component: ContactComponent},
+    
+  ])   ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, LibraryComponent, ContactComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
